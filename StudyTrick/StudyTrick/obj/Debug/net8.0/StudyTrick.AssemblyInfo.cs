bf6f7d22af29efe125f2c43ff7bdba2aa55ad3f4
aasthabhojani1104 <aasthabhojani26@gmail.com>
2025-03-12 09:43:48 +0530

    Add Program

--- StudyTrick/StudyTrick/obj/Debug/net8.0/StudyTrick.AssemblyInfo.cs
+++ StudyTrick/StudyTrick/obj/Debug/net8.0/StudyTrick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyTrick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614227f28b05c87153cce80fc45e904042cda0df")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyTrick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyTrick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
